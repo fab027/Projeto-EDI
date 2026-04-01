9450f0996e86fc82e528cad759caa68380b8142a
Fabricio Nascimento <129768096+fab027@users.noreply.github.com>
2026-04-01 08:27:53 -0300

    Update fila_compradores.h

--- include/fila_compradores.h
+++ include/fila_compradores.h
@@ -5,18 +5,16 @@
 
 void fila_compradores_inicializar(FilaCompradores *f);
 
-/* Enfileira cliente que comprou (enqueue) */
 void fila_compradores_enqueue(FilaCompradores *f,
                               const NoPotencial *base,
                               Data data_nasc,
                               const Endereco *end,
                               const char *cpf);
 
-/* Remove o primeiro da fila (logística processa em ordem de chegada) */
 NoComprador *fila_compradores_dequeue(FilaCompradores *f);
 
 int fila_compradores_tamanho(const FilaCompradores *f);
 
 void fila_compradores_destruir(FilaCompradores *f);
 
-#endif /* FILA_COMPRADORES_H */
+#endif 
