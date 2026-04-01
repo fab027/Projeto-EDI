0918d34f3186c5e54c9d173a0c8628bfbd37383a
Fabricio Nascimento <129768096+fab027@users.noreply.github.com>
2026-04-01 08:29:34 -0300

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -6,18 +6,14 @@
 #include "fila_compradores.h"
 #include "pilha_arquivados.h"
 
-/* Exibe o menu principal e retorna a opção escolhida (1-4) */
 int menu_principal(void);
 
-/* Cadastra novo potencial cliente */
 void menu_cadastrar(FilaEspera *fila);
 
-/* Fluxo completo de "Realizar Contato" */
 void menu_realizar_contato(FilaEspera *fila,
                            FilaCompradores *compradores,
                            PilhaArquivados *arquivados);
 
-/* Sub-menu de Relatórios */
 void menu_relatorios(const FilaEspera *fila);
 
-#endif /* MENU_H */
+#endif 
